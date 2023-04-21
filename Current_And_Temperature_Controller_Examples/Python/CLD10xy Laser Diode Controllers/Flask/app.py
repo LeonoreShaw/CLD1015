c840605595aac9c7baa7e3e9efc8b3f78701f519
LeonoreShaw <leonoreshaw@gmail.com>
2023-04-21 15:37:21 +0800

    实现开关LD，修改前端样式。

--- Current_And_Temperature_Controller_Examples/Python/CLD10xy Laser Diode Controllers/Flask/app.py
+++ Current_And_Temperature_Controller_Examples/Python/CLD10xy Laser Diode Controllers/Flask/app.py
@@ -1,33 +1,36 @@
 from flask import Flask, render_template, request
 import pyvisa
 
-# 创建资源管理器对象
-rm = pyvisa.ResourceManager()
-
 app = Flask(__name__)
 
+# Open the connection to the device
+rm = pyvisa.ResourceManager()
+# instr = rm.open_resource('USB0::0x1313::0x804F::M00295819::INSTR')
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    # Get the current setpoint
+    # setpoint = instr.query("source1:current:level:amplitude?")
+    return render_template('index.html', setpoint=1)
 
+@app.route('/set_setpoint', methods=['POST'])
+def set_setpoint():
+    # Set the new setpoint
+    setpoint = request.form['setpoint']
+    # instr.write("source1:current:level:amplitude {}".format(setpoint))
+    return "Setpoint set to {}".format(setpoint)
 
-@app.route("/connect", methods=["POST"])
-def connect_device():
-    # 连接设备
-    # instr = rm.open_resource('USB0::0x1313::0x804F::M00295819::INSTR')
+@app.route('/turn_on')
+def turn_on():
+    # Turn on the LD
+    # instr.write("output1:state on")
+    return "LD turned on"
 
-    # 设置电流
-    # instr.write("source1:current:level:amplitude 0.065")
-    # current = instr.query("source1:current:level:amplitude?")
+@app.route('/turn_off')
+def turn_off():
+    # Turn off the LD
+    # instr.write("output1:state off")
+    return "LD turned off"
 
-    # 关闭资源
-    # instr.close()
-    rm.close()
-
-    # 将结果发送回前端
-    return '0.065'
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
